8825f7187df5ccaa400ae9558d2fb28a19c94f79
Simon Wilkinson <sw561@cam.ac.uk>
2019-12-09 15:38:52 +0000

    Day 1 tidy

--- 01_fuel_required/fuel.py
+++ 01_fuel_required/fuel.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 
 def fuel(mass):
-    return mass // 3 - 2
-
-def part1(masses):
-    return sum(fuel(m) for m in masses)
+    return max(0, mass // 3 - 2)
 
 def fuel_part2(mass):
-    # Calculate addition fuel needed for carrying fuel
-    fuel_to_add = fuel(mass)
-    total = fuel_to_add
-    while fuel_to_add > 0:
-        fuel_to_add = max(0, fuel(fuel_to_add))
-        total += fuel_to_add
-
+    # Calculate additional fuel needed for carrying fuel
+    total = 0
+    while mass > 0:
+        mass = fuel(mass)
+        total += mass
     return total
-
-def part2(masses):
-    return sum(fuel_part2(m) for m in masses)
 
 if __name__=="__main__":
     with open("01_fuel_required/input.txt", 'r') as f:
-        masses = [int(x) for x in f]
+        masses = [int(x.strip()) for x in f]
 
-    print(part1(masses))
-    print(part2(masses))
+    print(sum(fuel(m) for m in masses))
+    print(sum(fuel_part2(m) for m in masses))
